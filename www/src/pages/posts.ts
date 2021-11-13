38f0214e40d51952ef53ef127727e1235f37c730
Robin Métral <robin@metral.ch>
2021-11-13 09:48:46 +0100

    Monorepo (#1)
    * Move example site into monorepo
    
    * Add e2e tests
    
    * Use Node 16
    
    Node 14 is still on npm 6 by default and doesn't have monorepo support
    
    * Install system deps for headless webkit in CI
    
    * Fix playwright install step

---
+++ www/src/pages/posts.ts
@@ -0,0 +1,53 @@
+import { readdir, readFile } from "fs/promises";
+import { load } from "js-yaml";
+
+const POSTS_DIR = "./src/posts";
+
+type Frontmatter = { [key: string]: string };
+
+function getFrontmatter(file: string): Frontmatter {
+  const match =
+    /^---(?:\r?\n|\r)(?:([\s\S]*?)(?:\r?\n|\r))?---(?:\r?\n|\r|$)/.exec(file);
+
+  if (match) {
+    return load(match[1]) as Frontmatter;
+  } else {
+    return {};
+  }
+}
+
+type Post = { title: string; date: string; slug: string };
+
+async function getPosts(): Promise<Post[]> {
+  const files = await readdir(POSTS_DIR);
+  const posts: Post[] = await Promise.all(
+    files.map(async (file) => {
+      const content = await readFile(`${POSTS_DIR}/${file}`, "utf-8");
+      const frontmatter = getFrontmatter(content);
+      return {
+        title: frontmatter.title,
+        date: new Date(frontmatter.date).toLocaleDateString(),
+        slug: `/posts/${file.replace(".md", "")}`,
+      };
+    })
+  );
+  return posts;
+}
+
+export async function buildPage(html: string): Promise<string> {
+  try {
+    const posts = await getPosts();
+    const postsHtml = posts
+      .map(
+        (post) =>
+          `<li><a href="${post.slug}">${post.date}: ${post.title}</a></li>`
+      )
+      .join("");
+    return html.replace(
+      '<ul id="posts" />',
+      `<ul id="posts">${postsHtml}</ul>`
+    );
+  } catch (error) {
+    throw new Error(`Failed to build page: ${error}`);
+  }
+}
