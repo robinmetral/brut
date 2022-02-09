2d42b063ec910fa7026eb088064f3abed26af958
Robin Métral <robin.metral@sumup.com>
2022-02-09 23:52:43 +0100

    Add source

--- brut/entry.sh
+++ brut/entry.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# adapted from https://stackoverflow.com/questions/70496353/how-to-get-yargs-auto-complete-working-when-using-experimental-specifier-reso
 full_path=$(realpath $0)
 dir_path=$(dirname $full_path)
 script_path="$dir_path/dist/index.js"
